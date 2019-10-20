1fe9560c71ba4317fd5a9bbe853de36896369d60
deank <dean1222@xtra.co.nz>
2019-10-20 23:31:15 +1300

    countPSIDHBs back 4y

--- countKPIs.js
+++ countKPIs.js
@@ -243,7 +243,7 @@ function countPSI(patients) {
 function countPSIDHBs(patients) {
   var dhbs = ["ADHB", "WDHB", "CMDHB", "Metro", "Northland", "Waikato", "Lakes", "BOP", "Tairawhiti", "Taranaki", "NonMetro", "Any"];
   var timeframes = [];
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 4; i++) {
     timeframes.push({
       period: "fiscalYears",
       steps: -i

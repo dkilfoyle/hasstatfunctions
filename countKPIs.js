57f823d78be9563bb29361b28b073bff8203b0c5
deank <dean1222@xtra.co.nz>
2019-11-05 16:58:31 +1300

    spelling mistake

--- countKPIs.js
+++ countKPIs.js
@@ -428,7 +428,7 @@ function weeklyReportData(patients) {
 }
 
 const titleColor = (backgroundColor) => {
-  const hex = backgroundColour.replace(/#/, '');
+  const hex = backgroundColor.replace(/#/, '');
   const r = parseInt(hex.substr(0, 2), 16);
   const g = parseInt(hex.substr(2, 2), 16);
   const b = parseInt(hex.substr(4, 2), 16);
